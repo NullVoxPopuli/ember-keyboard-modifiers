f86e6a402c71697ca39676559747753795f0523a
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-02-27 22:13:24 -0500

    I think on-key-press is in a good spot, just need to test it now :)

--- addon/modifiers/on-key-press.ts
+++ addon/modifiers/on-key-press.ts
@@ -1,39 +1,80 @@
 import Ember from 'ember';
 import { assert } from '@ember/debug';
+import ApplicationInstance from '@ember/application/instance';
+import Service from '@ember/service';
 
-interface IState {
-  element: Element | null;
-  shortcut?: string;
-  callback?: (element: Element) => void;
-  eventOptions?: any;
+type ModifierFunc = (element: Element) => void;
+
+interface IModifierArgs {
+  positional: [string, ModifierFunc];
+  named: any;
 }
 
-export default Ember._setModifierManager(
-  () => ({
-    createModifier(): IState {
-      return {
-        element: null,
-        shortcut: undefined,
-        callback: undefined,
-        eventOptions: undefined,
-      };
+
+interface IModifierManagerFactory<TArgs> {
+  createModifier(factory: any, args: TArgs): KeyboardModifier;
+  installModifier(instance: KeyboardModifier, element: Element, args: TArgs): void;
+  updateModifier(instance: KeyboardModifier, args: TArgs): void;
+  destroyModifier(instance: KeyboardModifier, args: TArgs): void;
+}
+
+// state;
+class KeyboardModifier {
+  element: Element | null;
+  shortcut?: string;
+  callback?: ModifierFunc;
+  eventOptions: any;
+  keyboardService: typeof Service;
+
+  constructor(args: IModifierArgs, keyboardService: typeof Service) {
+    this.element = null;
+    this.keyboardService = keyboardService;
+  }
+}
+
+function createManager(owner: ApplicationInstance): IModifierManagerFactory<IModifierArgs> {
+  return {
+    createModifier(factory, args) {
+      let keyboardService = owner.lookup('service:keyboard');
+
+      assert(`keyboard service not found. do you have ember-keyboard installed?`, keyboardService);
+
+      return factory.create(args, keyboardService);
     },
 
-    installModifier(state: IState, element: Element, { positional: [shortcut, callback], named: eventOptions }) {
+    installModifier(
+      instance,
+      element, {
+        positional: [shortcut, callback],
+        named: eventOptions
+      }) {
       assert(`callback in on-key-press modifier must be a function`, typeof callback === 'function');
       // TODO: assert valid shortcuts / key formats or something
+      // TODO: global config for asserting against common a11y tools? (don't override those)
 
-      state.element = element;
-      state.shortcut = shortcut;
-      state.callback = callback;
-      state.eventOptions = eventOptions;
+      instance.element = element;
+      instance.shortcut = shortcut;
+      instance.callback = callback;
+      instance.eventOptions = eventOptions;
+
+      instance.keyboardService.register(instance);
     },
 
-    updateModifier() { },
+    updateModifier(
+      instance, {
+      positional: [shortcut, callback],
+      named: eventOptions
+    }) {
+      instance.shortcut = shortcut;
+      instance.callback = callback;
+      instance.eventOptions = eventOptions;
+    },
 
-    destroyModifier() { },
-  }),
-  class OnKeyPressModifier {
-
+    destroyModifier(instance) {
+      instance.keyboardService.unregister(instance);
+    },
   }
-)
+}
+
+// https://github.com/emberjs/rfcs/blob/master/text/0373-Element-Modifier-Managers.md
+export default Ember._setModifierManager(createManager, KeyboardModifier);
